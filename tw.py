cefe1e77bdac7f4f9785cf2ca44084305bc6098c
Jonathan Senecal <jsenecal@connectitnet.com>
2019-02-04 13:32:38 -0500

    Better doc

--- tw.py
+++ tw.py
@@ -12,11 +12,11 @@ logger = logging.getLogger()
 @click.argument('input_file', type=click.File(mode='r'))
 @click.argument('template_file', type=click.File(mode='r', lazy=True))
 @click.argument('output_path', type=click.Path(exists=True, writable=True), default=lambda: os.getcwd())
-@click.option('--output_filename')
+@click.option('--output_filename', help='Jinja2 template string to generate filenames')
 @click.option('--delimiter', default=',')
 def cli(input_file, output_path, template_file, output_filename, delimiter):
     searchpath = os.path.dirname(template_file.name)
-    file_extension = os.path.splitext(template_file.name)[-1]
+    template_extension = os.path.splitext(template_file.name)[-1]
 
     env = jinja2.Environment(
         loader=jinja2.FileSystemLoader(searchpath=searchpath),
@@ -28,7 +28,7 @@ def cli(input_file, output_path, template_file, output_filename, delimiter):
     for row in data.itertuples():
         values = row._asdict()
         if not output_filename:
-            filename = str(row.Index) + file_extension
+            filename = str(row.Index) + template_extension
         else:
             filename = jinja2.Environment().from_string(output_filename).render(**values)
         output_file = open(output_path + filename, mode='w')
